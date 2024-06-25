c0324a0ff6be8c500da938d470f0a2391759b386
Thibaud P <25123473+ThobsChoucroute@users.noreply.github.com>
2024-06-25 22:54:34 +0200

    Add SuccessButton

---
+++ resources/js/Components/Forms/SuccessButton.vue
@@ -0,0 +1,43 @@
+<script setup>
+import SvgIcon from '@/Components/Ui/SvgIcon.vue';
+
+const props = defineProps({
+    icon: {
+        type: String
+    },
+    loading: {
+        type: Boolean,
+        default: false
+    }
+});
+</script>
+
+<template>
+    <button
+        type="button"
+        class="
+        inline-flex
+        items-center
+        px-3
+        py-2
+        text-base
+        font-medium
+        text-center
+        text-white
+        bg-green-700
+        rounded-lg
+        hover:bg-green-800
+        focus:ring-4
+        focus:ring-green-300
+        sm:w-auto
+        dark:bg-green-600
+        dark:hover:bg-green-700
+        dark:focus:ring-green-800
+        transition
+        ease-in-out
+        duration-150"
+    >
+        <SvgIcon class="w-4 h-4" v-if="icon" :icon="icon" :loading="loading" />
+        <slot/>
+    </button>
+</template>

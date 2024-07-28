ec7adfbe0ebd5327d3464e4d719b8555d534f08d
Thibaud P <25123473+ThobsChoucroute@users.noreply.github.com>
2024-07-28 23:24:03 +0200

    add font awesome brands icons

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,9 +8,11 @@ import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 library.add(fas);
+library.add(fab);
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 

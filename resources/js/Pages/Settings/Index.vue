f10808190e69e44803a059b82a1b318747b768c7
Thibaud P <25123473+ThobsChoucroute@users.noreply.github.com>
2024-07-29 15:23:01 +0200

    fix component not available yet

--- resources/js/Pages/Settings/Index.vue
+++ resources/js/Pages/Settings/Index.vue
@@ -6,7 +6,8 @@ import PageTitle from '@/Components/Ui/PageTitle.vue';
 import SvgIcon from '@/Components/Ui/SvgIcon.vue';
 import Tabs from '@/Components/Ui/Tabs.vue';
 import TabsVertical from '@/Components/Ui/TabsVertical.vue';
-import StripeForm from './Partials/StripeForm.vue';
+// import StripeForm from './Partials/StripeForm.vue';
+import RulesForm from './Partials/RulesForm.vue';
 
 defineOptions({layout: AuthenticatedLayout});
 
@@ -17,15 +18,20 @@ let tabs = [
     //     icon: "stripe-s",
     //     library: "fab"
     // },
-    // {
-    //     id: "rules",
-    //     label: "Règlement",
-    //     icon: "align-left"
-    // },
+    {
+        id: "rules",
+        label: "Règlement",
+        icon: "align-left"
+    },
     // {
     //     id: "cron",
     //     label: "Jobs",
     //     icon: "clock-rotate-left"
+    // },
+    // {
+    //     id: "log",
+    //     label: "Logs",
+    //     icon: "book",
     // },
     // {
     //     id: "settings",
@@ -43,13 +49,9 @@ let tabs = [
     <TabsVertical tabId="settingsTabs" :tabs="tabs">
         <!-- <template #stripe>
             <StripeForm/>
-        </template>
-        <template #rules>
-            <h3 class="text-lg font-bold text-gray-900 dark:text-white mb-2">Règlement Tab</h3>
-            <p class="mb-2">This is some placeholder content the Profile tab's associated content, clicking another tab will toggle the visibility of this one for the next.</p>
-            <p>The tab JavaScript swaps classes to control the content visibility and styling.</p>
-        </template>
-        <template #settings>
         </template> -->
+        <template #rules>
+            <RulesForm/>
+        </template>
     </TabsVertical>
 </template>

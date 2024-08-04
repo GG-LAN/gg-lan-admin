78c99d2d684ebf33bdd387196e6a79c31f070f55
Thibaud P <25123473+ThobsChoucroute@users.noreply.github.com>
2024-08-04 14:51:46 +0200

    add user icon generator

--- resources/js/Components/Navs/Navbar.vue
+++ resources/js/Components/Navs/Navbar.vue
@@ -40,7 +40,11 @@ const logout = () => {
                     <div class="flex items-center ml-3 gap-4">
                         <!-- Avatar button -->
                         <button type="button" class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button-2" aria-expanded="false" data-dropdown-toggle="dropdown-profile">
-                            <img class="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="user photo">
+                            <img
+                                class="w-8 h-8 rounded-full"
+                                :src="'https://api.dicebear.com/9.x/bottts-neutral/svg?seed=' + $page.props.auth.user.pseudo"
+                                alt="avatar"
+                            />
                         </button>
                         
                         <!-- Dropdown menu -->

18e297cd7638a986af1853234a336f25c2f2284e
Thibaud P <25123473+ThobsChoucroute@users.noreply.github.com>
2024-08-08 00:53:15 +0200

    Revert "Remove unused code that apparently seems to pose a problem now ??"
    This reverts commit e12f5fb66a77f4c6329ce9a96a4ae16d9dbb602c.

--- routes/auth.php
+++ routes/auth.php
@@ -4,11 +4,18 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\EmailVerificationPromptController;
+use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 
 Route::middleware('guest')->group(function () {
+    Route::controller(RegisteredUserController::class)->group(function () {
+        Route::get('register', 'create')->name('register');
+        Route::post('register', 'store');
+    });
 
     Route::controller(AuthenticatedSessionController::class)->group(function () {
         Route::get('login', 'create')->name('login');
@@ -27,6 +34,17 @@ Route::middleware('guest')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
+    Route::get('verify-email', EmailVerificationPromptController::class)
+        ->name('verification.notice');
+
+    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
+        ->middleware(['signed', 'throttle:6,1'])
+        ->name('verification.verify');
+
+    Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+        ->middleware('throttle:6,1')
+        ->name('verification.send');
+
     Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
         ->name('password.confirm');
 

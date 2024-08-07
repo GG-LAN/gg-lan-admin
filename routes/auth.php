3999fb5247a70edf227ebf9390bd72ef841100e8
Thibaud P <25123473+ThobsChoucroute@users.noreply.github.com>
2024-08-08 00:57:04 +0200

    Remove Register component because don't need it ?

--- routes/auth.php
+++ routes/auth.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\VerifyEmailController;
-use App\Http\Controllers\Auth\RegisteredUserController;
+// use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
@@ -12,10 +12,10 @@ use App\Http\Controllers\Auth\EmailVerificationPromptController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 
 Route::middleware('guest')->group(function () {
-    Route::controller(RegisteredUserController::class)->group(function () {
-        Route::get('register', 'create')->name('register');
-        Route::post('register', 'store');
-    });
+    // Route::controller(RegisteredUserController::class)->group(function () {
+    //     Route::get('register', 'create')->name('register');
+    //     Route::post('register', 'store');
+    // });
 
     Route::controller(AuthenticatedSessionController::class)->group(function () {
         Route::get('login', 'create')->name('login');

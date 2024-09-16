c9156db2b5ebae30128c9feb7d380855f9e2d49f
Thibaud P <25123473+ThobsChoucroute@users.noreply.github.com>
2024-09-16 20:43:38 +0200

    fix translation keys

--- app/Http/Controllers/Api/TournamentController.php
+++ app/Http/Controllers/Api/TournamentController.php
@@ -1,46 +1,51 @@
 <?php
 namespace App\Http\Controllers\Api;
 
-use Auth;
-use App\Models\User;
-use App\Models\Tournament;
 use App\Helpers\ApiResponse;
-use App\Models\PurchasedPlace;
-use App\Models\TournamentPrice;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\Storage;
 use App\Http\Requests\Tournaments\GetPaymentLinkRequest;
+use App\Models\PurchasedPlace;
+use App\Models\Tournament;
+use App\Models\TournamentPrice;
+use App\Models\User;
+use Auth;
+use Illuminate\Http\JsonResponse;
 
-class TournamentController extends Controller {
+class TournamentController extends Controller
+{
 
-    public function __construct() {
+    public function __construct()
+    {
 
     }
 
     /**
      * Return all tournaments
      */
-    public function index() {
+    public function index()
+    {
         return ApiResponse::success("", Tournament::without('players', 'teams')->get());
     }
 
     /**
      * Return a paginate version of all the tournaments
      */
-    public function index_paginate($item_per_page) {
+    public function index_paginate($item_per_page)
+    {
         return ApiResponse::success("", Tournament::without('players', 'teams')->paginate($item_per_page));
     }
 
     /**
      * Return a tournament
      */
-    public function show(Tournament $tournament): JsonResponse {
+    public function show(Tournament $tournament): JsonResponse
+    {
         return ApiResponse::success("", $tournament);
     }
 
-    public function showPurchasedPlaces(Tournament $tournament): JsonResponse {
+    public function showPurchasedPlaces(Tournament $tournament): JsonResponse
+    {
         if (!$tournament) {
-            return ApiResponse::unprocessable(__("responses.tournaments.not_exists"), []);
+            return ApiResponse::unprocessable(__("responses.tournament.not_exists"), []);
         }
 
         return ApiResponse::success("", $tournament->purchasedPlaces());
@@ -49,60 +54,63 @@ class TournamentController extends Controller {
     /**
      * Register a player in the tournament
      */
-    public function register(Tournament $tournament, User $player): JsonResponse {
+    public function register(Tournament $tournament, User $player): JsonResponse
+    {
         // If the tournament is a team tournament
-        if($tournament->type == "team") {
-            return ApiResponse::forbidden(__('responses.tournaments.not_solo') , []);
+        if ($tournament->type == "team") {
+            return ApiResponse::forbidden(__('responses.tournament.not_solo'), []);
         }
 
         // If the tournament is full
         if ($tournament->isFull) {
-            return ApiResponse::forbidden(__("responses.tournaments.full"), []);
+            return ApiResponse::forbidden(__("responses.tournament.full"), []);
         }
-        
+
         // If the auth user is not the same user in request AND it's not an auth admin
-        if(Auth::user()->id != $player->id && !Auth::user()->admin) {
-            return ApiResponse::forbidden(__("responses.tournaments.player_not_you"), []);
+        if (Auth::user()->id != $player->id && !Auth::user()->admin) {
+            return ApiResponse::forbidden(__("responses.tournament.player_not_you"), []);
         }
-        
-        if(!$tournament->checkPlayerIsRegistered($player)) {
+
+        if (!$tournament->checkPlayerIsRegistered($player)) {
             $tournament->players()->attach($player);
             PurchasedPlace::register($player, $tournament);
-            
-            return ApiResponse::success(__("responses.tournaments.registered"), $tournament);
+
+            return ApiResponse::success(__("responses.tournament.registered"), $tournament);
         }
-        
-        return ApiResponse::forbidden(__("responses.tournaments.already_registered"), []);
+
+        return ApiResponse::forbidden(__("responses.tournament.already_registered"), []);
     }
 
     /**
      * Unregister a player in the tournament
      */
-    public function unregister(Tournament $tournament, User $player): JsonResponse {        
-        if(Auth::user()->id != $player->id && !Auth::user()->admin) {
-            return ApiResponse::forbidden(__("responses.tournaments.player_not_you"), []);
+    public function unregister(Tournament $tournament, User $player): JsonResponse
+    {
+        if (Auth::user()->id != $player->id && !Auth::user()->admin) {
+            return ApiResponse::forbidden(__("responses.tournament.player_not_you"), []);
         }
 
-        if($tournament->checkPlayerIsRegistered($player)) {
+        if ($tournament->checkPlayerIsRegistered($player)) {
             $tournament->players()->detach($player);
             PurchasedPlace::unregister($player, $tournament);
-    
-            return ApiResponse::success(__("responses.tournaments.unregistered"), $tournament);
+
+            return ApiResponse::success(__("responses.tournament.unregistered"), $tournament);
         }
 
-        return ApiResponse::forbidden(__("responses.tournaments.not_registered"), []);
+        return ApiResponse::forbidden(__("responses.tournament.not_registered"), []);
     }
 
     /**
      * Return all the price for each open tournaments
      */
-    public function prices(): JsonResponse {
+    public function prices(): JsonResponse
+    {
         $tournaments = Tournament::where("status", "open")->get();
         $prices = [];
 
         foreach ($tournaments as $key => $tournament) {
             $currentPrice = $tournament->currentPrice();
-                        
+
             if (!$currentPrice) {
                 continue;
             }
@@ -117,9 +125,10 @@ class TournamentController extends Controller {
         return ApiResponse::success("", $prices);
     }
 
-    public function getPaymentLink(GetPaymentLinkRequest $request, Tournament $tournament) {
+    public function getPaymentLink(GetPaymentLinkRequest $request, Tournament $tournament)
+    {
         return ApiResponse::success("", [
-            "payment_url" => $tournament->getPaymentLink($request)
+            "payment_url" => $tournament->getPaymentLink($request),
         ]);
     }
 }
--- tests/Feature/TournamentTest.php
+++ tests/Feature/TournamentTest.php
@@ -1,22 +1,18 @@
 <?php
 
-use App\ApiCode;
-use App\Models\Game;
-use App\Models\Team;
-use App\Models\User;
-use App\Models\Tournament;
 use App\Models\PurchasedPlace;
+use App\Models\Tournament;
 use App\Models\TournamentPrice;
-use Stripe\StripeClient as Stripe;
-    
+use App\Models\User;
+
 it("can_get_tournaments", function () {
     $tournaments = Tournament::factory(10)->create();
 
     $this->get('/api/tournaments')
-    ->assertOk()
-    ->assertJson([
-        "data" => $tournaments->toArray()
-    ]);
+        ->assertOk()
+        ->assertJson([
+            "data" => $tournaments->toArray(),
+        ]);
 });
 
 it("can_get_tournaments_with_pagination", function () {
@@ -24,11 +20,11 @@ it("can_get_tournaments_with_pagination", function () {
 
     $item_per_page = 5;
     $tournaments = Tournament::paginate($item_per_page);
-    
-    $response = $this->get('/api/tournaments/paginate/'.$item_per_page);
+
+    $response = $this->get('/api/tournaments/paginate/' . $item_per_page);
 
     $result = json_decode($response->getContent(), true);
-    
+
     $response->assertOk();
 
     $this->assertEquals($item_per_page, count($result['data']['data']));
@@ -37,158 +33,158 @@ it("can_get_tournaments_with_pagination", function () {
 it("can_get_tournament", function () {
     $tournament = Tournament::factory()->create();
 
-    $this->get('/api/tournaments/'.$tournament->id)
-    ->assertOk()
-    ->assertJson([
-        "data" => $tournament->toArray()
-    ]);
+    $this->get('/api/tournaments/' . $tournament->id)
+        ->assertOk()
+        ->assertJson([
+            "data" => $tournament->toArray(),
+        ]);
 });
 
 it("can_add_player_to_tournament_of_type_solo", function () {
     $tournament = Tournament::factory()
-    ->has(TournamentPrice::factory()->state(function (array $attributes, Tournament $tournament) {
-        return [
-            'name' => $tournament->name,
-            'tournament_id' => $tournament->id,
-            'type' => 'normal',
-            'active' => true
-        ];
-    }), "prices")
-    ->create([
-        'type' => 'solo',
-        'status' => 'open'
-    ]);
+        ->has(TournamentPrice::factory()->state(function (array $attributes, Tournament $tournament) {
+            return [
+                'name' => $tournament->name,
+                'tournament_id' => $tournament->id,
+                'type' => 'normal',
+                'active' => true,
+            ];
+        }), "prices")
+        ->create([
+            'type' => 'solo',
+            'status' => 'open',
+        ]);
 
     $user = User::factory()->create();
 
-    $this->actingAs($user)->post('/api/tournaments/'.$tournament->id.'/register/'.$user->id)
-    ->assertOk()
-    ->assertJson([
-        'status'  => "success",
-        'message' => __("responses.tournaments.registered")
-    ]);
+    $this->actingAs($user)->post('/api/tournaments/' . $tournament->id . '/register/' . $user->id)
+        ->assertOk()
+        ->assertJson([
+            'status' => "success",
+            'message' => __("responses.tournament.registered"),
+        ]);
 });
 
 it("can_add_player_to_tournament_of_type_solo_as_admin", function () {
     $tournament = Tournament::factory()
-    ->has(TournamentPrice::factory()->state(function (array $attributes, Tournament $tournament) {
-        return [
-            'name' => $tournament->name,
-            'tournament_id' => $tournament->id,
-            'type' => 'normal',
-            'active' => true
-        ];
-    }), "prices")
-    ->create([
-        'type' => 'solo',
-        'status' => 'open'
-    ]);
+        ->has(TournamentPrice::factory()->state(function (array $attributes, Tournament $tournament) {
+            return [
+                'name' => $tournament->name,
+                'tournament_id' => $tournament->id,
+                'type' => 'normal',
+                'active' => true,
+            ];
+        }), "prices")
+        ->create([
+            'type' => 'solo',
+            'status' => 'open',
+        ]);
 
     $user = User::factory()->create();
     $admin = User::factory()->create([
-        'admin' => 1
+        'admin' => 1,
     ]);
 
-    $this->actingAs($admin)->post('/api/tournaments/'.$tournament->id.'/register/'.$user->id)
-    ->assertOk()
-    ->assertJson([
-        'status'  => "success",
-        'message' => __("responses.tournaments.registered")
-    ]);
+    $this->actingAs($admin)->post('/api/tournaments/' . $tournament->id . '/register/' . $user->id)
+        ->assertOk()
+        ->assertJson([
+            'status' => "success",
+            'message' => __("responses.tournament.registered"),
+        ]);
 });
 
 it("cant_add_another_player_to_tournament_of_type_solo", function () {
     $tournament = Tournament::factory()
-    ->create([
-        'type' => 'solo',
-        'status' => 'open'
-    ]);
+        ->create([
+            'type' => 'solo',
+            'status' => 'open',
+        ]);
 
     $user = User::factory()->create();
     $other_user = User::factory()->create();
 
-    $this->actingAs($user)->post('/api/tournaments/'.$tournament->id.'/register/'.$other_user->id)
-    ->assertForbidden()
-    ->assertJson([
-        'status'  => "error",
-        'message' => __("responses.tournaments.player_not_you")
-    ]);
+    $this->actingAs($user)->post('/api/tournaments/' . $tournament->id . '/register/' . $other_user->id)
+        ->assertForbidden()
+        ->assertJson([
+            'status' => "error",
+            'message' => __("responses.tournament.player_not_you"),
+        ]);
 });
 
 it("can_remove_player_from_tournament_of_type_solo", function () {
     $tournament = Tournament::factory()
-    ->has(TournamentPrice::factory()->state(function (array $attributes, Tournament $tournament) {
-        return [
-            'name' => $tournament->name,
-            'tournament_id' => $tournament->id,
-            'type' => 'normal',
-            'active' => true
-        ];
-    }), "prices")
-    ->create([
-        'type' => 'solo',
-        'status' => 'open'
-    ]);
+        ->has(TournamentPrice::factory()->state(function (array $attributes, Tournament $tournament) {
+            return [
+                'name' => $tournament->name,
+                'tournament_id' => $tournament->id,
+                'type' => 'normal',
+                'active' => true,
+            ];
+        }), "prices")
+        ->create([
+            'type' => 'solo',
+            'status' => 'open',
+        ]);
 
     $user = User::factory()->create();
 
     $tournament->players()->attach($user);
 
-    $this->actingAs($user)->post('/api/tournaments/'.$tournament->id.'/unregister/'.$user->id)
-    ->assertOk()
-    ->assertJson([
-        'status' => "success",
-        'message' => __("responses.tournaments.unregistered")
-    ]);
+    $this->actingAs($user)->post('/api/tournaments/' . $tournament->id . '/unregister/' . $user->id)
+        ->assertOk()
+        ->assertJson([
+            'status' => "success",
+            'message' => __("responses.tournament.unregistered"),
+        ]);
 });
 
 it("cant_remove_another_player_from_tournament_of_type_solo", function () {
     $tournament = Tournament::factory()
-    ->create([
-        'type' => 'solo',
-        'status' => 'open'
-    ]);
+        ->create([
+            'type' => 'solo',
+            'status' => 'open',
+        ]);
 
     $user = User::factory()->create();
     $other_user = User::factory()->create();
 
-    $this->actingAs($user)->post('/api/tournaments/'.$tournament->id.'/unregister/'.$other_user->id)
-    ->assertForbidden()
-    ->assertJson([
-        'status' => "error",
-        'message' => __("responses.tournaments.player_not_you")
-    ]);
+    $this->actingAs($user)->post('/api/tournaments/' . $tournament->id . '/unregister/' . $other_user->id)
+        ->assertForbidden()
+        ->assertJson([
+            'status' => "error",
+            'message' => __("responses.tournament.player_not_you"),
+        ]);
 });
 
 it("can_remove_player_from_tournament_of_type_solo_as_admin", function () {
     $tournament = Tournament::factory()
-    ->has(TournamentPrice::factory()->state(function (array $attributes, Tournament $tournament) {
-        return [
-            'name' => $tournament->name,
-            'tournament_id' => $tournament->id,
-            'type' => 'normal',
-            'active' => true
-        ];
-    }), "prices")
-    ->create([
-        'type' => 'solo',
-        'status' => 'open'
-    ]);
+        ->has(TournamentPrice::factory()->state(function (array $attributes, Tournament $tournament) {
+            return [
+                'name' => $tournament->name,
+                'tournament_id' => $tournament->id,
+                'type' => 'normal',
+                'active' => true,
+            ];
+        }), "prices")
+        ->create([
+            'type' => 'solo',
+            'status' => 'open',
+        ]);
 
     $user = User::factory()->create();
 
     $admin = User::factory()->create([
-        'admin' => 1
+        'admin' => 1,
     ]);
 
     $tournament->players()->attach($user);
 
-    $this->actingAs($admin)->post('/api/tournaments/'.$tournament->id.'/unregister/'.$user->id)
-    ->assertOk()
-    ->assertJson([
-        'status' => "success",
-        'message' => __("responses.tournaments.unregistered")
-    ]);
+    $this->actingAs($admin)->post('/api/tournaments/' . $tournament->id . '/unregister/' . $user->id)
+        ->assertOk()
+        ->assertJson([
+            'status' => "success",
+            'message' => __("responses.tournament.unregistered"),
+        ]);
 });
 
 it("get_tournament_purchased_places", function () {
@@ -197,27 +193,27 @@ it("get_tournament_purchased_places", function () {
     $tournament = Tournament::factory()->create();
 
     $tournamentPrice = TournamentPrice::factory()->create([
-        'tournament_id' => $tournament->id
+        'tournament_id' => $tournament->id,
     ]);
 
     $purchasedPlaces = PurchasedPlace::factory(4)->create([
-        'tournament_price_id' => $tournamentPrice->id
+        'tournament_price_id' => $tournamentPrice->id,
     ]);
-    
+
     $this->actingAs($user)->get("api/tournaments/" . $tournament->id . "/purchasedPlaces")
-    ->assertOk()
-    ->assertJson([
-        "data" => $purchasedPlaces->toArray()
-    ]);
+        ->assertOk()
+        ->assertJson([
+            "data" => $purchasedPlaces->toArray(),
+        ]);
 });
 
 it("cant_register_player_if_tournament_is_full", function () {
     $tournament = Tournament::factory()->create([
         'type' => 'solo',
         'status' => 'open',
-        'places' => 4
+        'places' => 4,
     ]);
-    
+
     $players = User::factory(4)->create();
     foreach ($players as $player) {
         $tournament->players()->attach($player);
@@ -225,36 +221,36 @@ it("cant_register_player_if_tournament_is_full", function () {
 
     $user = User::factory()->create();
 
-    $this->actingAs($user)->post('/api/tournaments/'.$tournament->id.'/register/'.$user->id)
-    ->assertForbidden()
-    ->assertJson([
-        'status'  => "error",
-        'message' => __("responses.tournaments.full")
-    ]);
+    $this->actingAs($user)->post('/api/tournaments/' . $tournament->id . '/register/' . $user->id)
+        ->assertForbidden()
+        ->assertJson([
+            'status' => "error",
+            'message' => __("responses.tournament.full"),
+        ]);
 });
 
 it("cant_register_player_to_a_team_tournament", function () {
     $tournament = Tournament::factory()->create([
         'type' => 'team',
         'status' => 'open',
-        'places' => 4
+        'places' => 4,
     ]);
 
     $user = User::factory()->create();
 
-    $this->actingAs($user)->post('/api/tournaments/'.$tournament->id.'/register/'.$user->id)
-    ->assertForbidden()
-    ->assertJson([
-        'status'  => "error",
-        'message' => __("responses.tournaments.not_solo")
-    ]);
+    $this->actingAs($user)->post('/api/tournaments/' . $tournament->id . '/register/' . $user->id)
+        ->assertForbidden()
+        ->assertJson([
+            'status' => "error",
+            'message' => __("responses.tournament.not_solo"),
+        ]);
 });
 
 it("cant_register_twice_player_to_same_tournament", function () {
     $tournament = Tournament::factory()->create([
         'type' => 'solo',
         'status' => 'open',
-        'places' => 4
+        'places' => 4,
     ]);
 
     $user = User::factory()->create();
@@ -263,19 +259,19 @@ it("cant_register_twice_player_to_same_tournament", function () {
     $tournament->players()->attach($user);
 
     // Second register
-    $this->actingAs($user)->post('/api/tournaments/'.$tournament->id.'/register/'.$user->id)
-    ->assertForbidden()
-    ->assertJson([
-        'status'  => "error",
-        'message' => __("responses.tournaments.already_registered")
-    ]);
+    $this->actingAs($user)->post('/api/tournaments/' . $tournament->id . '/register/' . $user->id)
+        ->assertForbidden()
+        ->assertJson([
+            'status' => "error",
+            'message' => __("responses.tournament.already_registered"),
+        ]);
 });
 
 it("cant_unregister_twice_player_to_same_tournament", function () {
     $tournament = Tournament::factory()->create([
         'type' => 'solo',
         'status' => 'open',
-        'places' => 4
+        'places' => 4,
     ]);
 
     $user = User::factory()->create();
@@ -284,36 +280,36 @@ it("cant_unregister_twice_player_to_same_tournament", function () {
     $tournament->players()->attach($user);
 
     // First unregister (valid)
-    $this->actingAs($user)->post('/api/tournaments/'.$tournament->id.'/unregister/'.$user->id);
+    $this->actingAs($user)->post('/api/tournaments/' . $tournament->id . '/unregister/' . $user->id);
 
     // Second register (invalid)
-    $this->actingAs($user)->post('/api/tournaments/'.$tournament->id.'/unregister/'.$user->id)
-    ->assertForbidden()
-    ->assertJson([
-        'status'  => "error",
-        'message' => __("responses.tournaments.not_registered")
-    ]);
+    $this->actingAs($user)->post('/api/tournaments/' . $tournament->id . '/unregister/' . $user->id)
+        ->assertForbidden()
+        ->assertJson([
+            'status' => "error",
+            'message' => __("responses.tournament.not_registered"),
+        ]);
 });
 
 test("A purchased place is created after registering a player to a tournament", function () {
     $tournament = Tournament::factory()
-    ->has(TournamentPrice::factory()->state(function (array $attributes, Tournament $tournament) {
-        return [
-            'name' => $tournament->name,
-            'tournament_id' => $tournament->id,
-            'type' => 'normal',
-            'active' => true
-        ];
-    }), "prices")
-    ->create([
-        'type' => 'solo',
-        'status' => 'open',
-        'places' => 4
-    ]);    
+        ->has(TournamentPrice::factory()->state(function (array $attributes, Tournament $tournament) {
+            return [
+                'name' => $tournament->name,
+                'tournament_id' => $tournament->id,
+                'type' => 'normal',
+                'active' => true,
+            ];
+        }), "prices")
+        ->create([
+            'type' => 'solo',
+            'status' => 'open',
+            'places' => 4,
+        ]);
 
     $user = User::factory()->create();
 
-    $this->actingAs($user)->post('/api/tournaments/'.$tournament->id.'/register/'.$user->id);
+    $this->actingAs($user)->post('/api/tournaments/' . $tournament->id . '/register/' . $user->id);
 
     $this->assertDatabaseHas("purchased_places", [
         "user_id" => $user->id,
@@ -324,25 +320,25 @@ test("A purchased place is created after registering a player to a tournament", 
 
 test("Purchased place is deleted after unregistering a player from a tournament", function () {
     $tournament = Tournament::factory()
-    ->has(TournamentPrice::factory()->state(function (array $attributes, Tournament $tournament) {
-        return [
-            'name' => $tournament->name,
-            'tournament_id' => $tournament->id,
-            'type' => 'normal',
-            'active' => true
-        ];
-    }), "prices")
-    ->create([
-        'type' => 'solo',
-        'status' => 'open',
-        'places' => 4
-    ]);    
+        ->has(TournamentPrice::factory()->state(function (array $attributes, Tournament $tournament) {
+            return [
+                'name' => $tournament->name,
+                'tournament_id' => $tournament->id,
+                'type' => 'normal',
+                'active' => true,
+            ];
+        }), "prices")
+        ->create([
+            'type' => 'solo',
+            'status' => 'open',
+            'places' => 4,
+        ]);
 
     $user = User::factory()->create();
 
     $tournament->players()->attach($user);
 
-    $this->actingAs($user)->post('/api/tournaments/'.$tournament->id.'/unregister/'.$user->id);
+    $this->actingAs($user)->post('/api/tournaments/' . $tournament->id . '/unregister/' . $user->id);
 
     $this->assertDatabaseMissing("purchased_places", [
         "user_id" => $user->id,

d3fc4bc798df37515413dbff1d19319677741d42
Thibaud P <25123473+ThobsChoucroute@users.noreply.github.com>
2024-09-18 00:47:04 +0200

    Fix responses

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -1,20 +1,22 @@
 <?php
 namespace App\Http\Controllers\Api;
 
-use Auth;
-use App\Models\Team;
-use App\Models\User;
 use App\Events\TeamUpdated;
 use App\Helpers\ApiResponse;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Users\UpdateUserRequest;
+use App\Models\Team;
+use App\Models\User;
+use Auth;
+use Illuminate\Http\Request;
 
-class UserController extends Controller {
+class UserController extends Controller
+{
 
     private $fieldsToShow;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->fieldsToShow = ['id', 'pseudo', 'image', 'created_at', 'updated_at'];
     }
 
@@ -23,28 +25,32 @@ class UserController extends Controller {
      *
      * @return array
      */
-    public function index() {
+    public function index()
+    {
         return ApiResponse::success("", User::all($this->fieldsToShow));
     }
 
     /**
      * Return a paginate version of all the players
      */
-    public function index_paginate(Request $request, $item_per_page) {
+    public function index_paginate(Request $request, $item_per_page)
+    {
         return ApiResponse::success("", User::paginate($item_per_page, $this->fieldsToShow));
     }
 
     /**
      * Return a player
      */
-    public function show(User $player) {
+    public function show(User $player)
+    {
         return ApiResponse::success("", $player->only($this->fieldsToShow));
     }
 
     /**
      * Update a user
      */
-    public function update(UpdateUserRequest $request, User $player) {
+    public function update(UpdateUserRequest $request, User $player)
+    {
         $player->update([
             "pseudo" => $request->pseudo,
             "birth_date" => $request->birth_date,
@@ -59,51 +65,54 @@ class UserController extends Controller {
      * @param int $id
      * @return \Illuminate\Http\Response
      */
-    public function delete(User $player) {
+    public function delete(User $player)
+    {
         if (Auth::user()->admin) {
             $player->delete();
 
             return ApiResponse::success(__("responses.users.deleted"), []);
-        }
-        else {
+        } else {
             return ApiResponse::forbidden(__("responses.users.only_admin"), []);
         }
     }
 
-    public function playerTournaments(User $player) {
+    public function playerTournaments(User $player)
+    {
         $tournaments = [];
-        
+
         $playerTournaments = $player->tournaments;
         foreach ($playerTournaments as $playerTournament) {
             array_push($tournaments, $playerTournament);
         }
-                
+
         $teams = $player->teams;
         foreach ($teams as $team) {
             array_push($tournaments, $team->tournament);
         }
-        
+
         return ApiResponse::success("", $tournaments);
     }
 
-    public function playerTeams(User $player) {
+    public function playerTeams(User $player)
+    {
         return ApiResponse::success("", $player->teams);
     }
 
-    public function leaveTeam(User $player, Team $team) {
+    public function leaveTeam(User $player, Team $team)
+    {
         // If the auth user isn't the same user that tries to leave the team
         if (Auth::user()->id != $player->id) {
             return ApiResponse::forbidden(__("responses.users.player_not_you"), []);
         }
-        
+
         // If the player isn't in the team
         if (!$team->users->where('id', $player->id)->first()) {
-            return ApiResponse::forbidden(__("responses.teams.player_not_in_team"), []);
+            return ApiResponse::forbidden(__("responses.team.player_not_in_team"), []);
         }
 
         // If the captain tries to leave the team
         if ($team->captain_id == $player->id) {
-            return ApiResponse::forbidden(__("responses.teams.captain_cant_leave"), []);
+            return ApiResponse::forbidden(__("responses.team.captain_cant_leave"), []);
         }
 
         $team->users()->detach($player);

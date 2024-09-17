52d57c24545062168a75d8f035bdbaaaa3605fb8
Thibaud P <25123473+ThobsChoucroute@users.noreply.github.com>
2024-09-17 10:34:54 +0200

    add function to translate in Front pages

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Http\Request;
 use Inertia\Middleware;
 use Tighten\Ziggy\Ziggy;
-use Illuminate\Http\Request;
 
 class HandleInertiaRequests extends Middleware
 {
@@ -18,7 +18,7 @@ class HandleInertiaRequests extends Middleware
     /**
      * Determine the current asset version.
      */
-    public function version(Request $request): string|null
+    public function version(Request $request): string | null
     {
         return parent::version($request);
     }
@@ -30,20 +30,31 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request): array
     {
+        $locale = config("app.locale");
+
         return [
-            ...parent::share($request),
+             ...parent::share($request),
             'auth' => [
                 'user' => $request->user(),
             ],
-            'ziggy' => fn () => [
-                ...(new Ziggy)->toArray(),
+            'ziggy' => fn() => [
+                 ...(new Ziggy)->toArray(),
                 'location' => $request->url(),
             ],
             'flash' => [
-                'status' => fn () => $request->session()->get('status'),
-                'message' => fn () => $request->session()->get('message')
+                'status' => fn() => $request->session()->get('status'),
+                'message' => fn() => $request->session()->get('message'),
             ],
-            "appName" => config("app.name")
+            "appName" => config("app.name"),
+            "localeFile" => function () use ($locale) {
+                $localeFile = base_path('lang/' . $locale . '/' . $locale . '.json');
+
+                if (!file_exists($localeFile)) {
+                    return [];
+                }
+
+                return json_decode(file_get_contents($localeFile), true);
+            },
         ];
     }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,27 +1,46 @@
-import './bootstrap';
-import '../css/app.css';
-import './fontawesome-import';
+import "./bootstrap";
+import "../css/app.css";
+import "./fontawesome-import";
 
-import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import { createApp, h } from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { ZiggyVue } from "../../vendor/tightenco/ziggy";
 
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
 createInertiaApp({
     title: (title) => `${appName} - ${title}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob("./Pages/**/*.vue")
+        ),
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
             .component("font-awesome-icon", FontAwesomeIcon)
+            .mixin({
+                methods: {
+                    __: (key) => {
+                        let localeFile = props.initialPage.props.localeFile;
+                        console.log(localeFile);
+
+                        if (!localeFile) return key;
+
+                        let translation =
+                            key in localeFile ? localeFile[key] : key;
+
+                        return translation;
+                    },
+                },
+            })
             .mount(el);
     },
     progress: {
-        color: '#4B5563',
+        color: "#4B5563",
     },
 });

944473dc4fa87657f13d213a850b9295928fe186
Thibaud P <25123473+ThobsChoucroute@users.noreply.github.com>
2024-09-17 20:19:14 +0200

    Update Edit.vue

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -1,12 +1,12 @@
 <script setup>
-import { Head } from '@inertiajs/vue3';
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import DeleteUserForm from './Partials/DeleteUserForm.vue';
-import UpdatePasswordForm from './Partials/UpdatePasswordForm.vue';
-import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm.vue';
-import PageTitle from '@/Components/Ui/PageTitle.vue';
+import { Head } from "@inertiajs/vue3";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import DeleteUserForm from "./Partials/DeleteUserForm.vue";
+import UpdatePasswordForm from "./Partials/UpdatePasswordForm.vue";
+import UpdateProfileInformationForm from "./Partials/UpdateProfileInformationForm.vue";
+import PageTitle from "@/Components/Ui/PageTitle.vue";
 
-defineOptions({layout: AuthenticatedLayout});
+defineOptions({ layout: AuthenticatedLayout });
 
 defineProps({
     mustVerifyEmail: {
@@ -19,18 +19,20 @@ defineProps({
 
 const breadcrumbs = [
     {
-        "label" : "Profil",
-        "route" : route('profile.edit'),
-        "active" : true
-    }
-]
+        label: "Profil",
+        route: route("profile.edit"),
+        active: true,
+    },
+];
 </script>
 
 <template>
     <Head title="Profil" />
-    <PageTitle title="Profil" :breadcrumbs="breadcrumbs"/>
+    <PageTitle title="Profil" :breadcrumbs="breadcrumbs" />
 
-    <div class="p-4 bg-white rounded-lg shadow-sm sm:p-6 dark:bg-gray-800 text-gray-900 dark:text-white col-span-4 lg:col-span-2">
+    <div
+        class="p-4 bg-white rounded-lg shadow-sm sm:p-6 dark:bg-gray-800 text-gray-900 dark:text-white col-span-4 lg:col-span-2"
+    >
         <UpdateProfileInformationForm
             :must-verify-email="mustVerifyEmail"
             :status="status"
@@ -38,11 +40,15 @@ const breadcrumbs = [
         />
     </div>
 
-    <div class="p-4 bg-white rounded-lg shadow-sm sm:p-6 dark:bg-gray-800 text-gray-900 dark:text-white col-span-4 lg:col-span-2">
+    <div
+        class="p-4 bg-white rounded-lg shadow-sm sm:p-6 dark:bg-gray-800 text-gray-900 dark:text-white col-span-4 lg:col-span-2"
+    >
         <UpdatePasswordForm class="max-w-xl" />
     </div>
 
-    <!-- <div class="p-4 bg-white rounded-lg shadow-sm sm:p-6 dark:bg-gray-800 text-gray-900 dark:text-white col-span-1">
+    <div
+        class="p-4 bg-white rounded-lg shadow-sm sm:p-6 dark:bg-gray-800 text-gray-900 dark:text-white col-span-1"
+    >
         <DeleteUserForm class="max-w-xl" />
-    </div> -->
+    </div>
 </template>

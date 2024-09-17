4a8fc892f00a117e2b05882b39e98982ced4aa5f
Thibaud P <25123473+ThobsChoucroute@users.noreply.github.com>
2024-09-17 11:48:39 +0200

    fix flowbite-vue style call

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ export default {
         "./storage/framework/views/*.php",
         "./resources/views/**/*.blade.php",
         "./node_modules/flowbite/**/*.js",
+        "./node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx,vue}",
         "./resources/js/**/*.vue",
     ],
 

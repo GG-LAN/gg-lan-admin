e4c81bc8008c62c76b9e32ee2b9feacbdf87c0a3
Thibaud P <25123473+ThobsChoucroute@users.noreply.github.com>
2024-09-17 11:43:09 +0200

    Formatting

--- resources/js/Components/Navs/Navbar.vue
+++ resources/js/Components/Navs/Navbar.vue
@@ -1,30 +1,48 @@
 <script setup>
-import { Link, router } from '@inertiajs/vue3';
-import SvgIcon from '@/Components/Ui/SvgIcon.vue';
-import ApplicationLogo from '@/Components/Ui/ApplicationLogo.vue';
-import DarkModeButton from '@/Components/Ui/DarkModeButton.vue';
+import { Link, router } from "@inertiajs/vue3";
+import SvgIcon from "@/Components/Ui/SvgIcon.vue";
+import ApplicationLogo from "@/Components/Ui/ApplicationLogo.vue";
+import DarkModeButton from "@/Components/Ui/DarkModeButton.vue";
 
 const props = defineProps({});
 
 const logout = () => {
     router.post("/logout");
-}
+};
 </script>
 
 <template>
-    <nav class="fixed z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+    <nav
+        class="fixed z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700"
+    >
         <div class="px-3 py-3 lg:px-5 lg:pl-3">
             <div class="flex items-center justify-between">
                 <div class="flex items-center justify-start">
-                    <button data-drawer-target="sidebar" data-drawer-toggle="sidebar" aria-controls="sidebar" type="button" class="inline-flex items-center p-2 ms-3 mr-2 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 z-40">
+                    <button
+                        data-drawer-target="sidebar"
+                        data-drawer-toggle="sidebar"
+                        aria-controls="sidebar"
+                        type="button"
+                        class="inline-flex items-center p-2 ms-3 mr-2 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 z-40"
+                    >
                         <span class="sr-only">Open sidebar</span>
-                        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+                        <svg
+                            class="w-6 h-6"
+                            aria-hidden="true"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                clip-rule="evenodd"
+                                fill-rule="evenodd"
+                                d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+                            ></path>
                         </svg>
                     </button>
-                    
+
                     <Link :href="route('dashboard')" class="flex ml-2 md:mr-24">
-                        <ApplicationLogo class="h-8 mr-4 bg-gray-800 pr-1"/>
+                        <ApplicationLogo class="h-8 mr-4 bg-gray-800 pr-1" />
                     </Link>
 
                     <!-- General Search Field -->
@@ -32,29 +50,46 @@ const logout = () => {
                 </div>
 
                 <div class="flex items-center">
-
                     <!-- Toggle Dark/Light mode -->
-                    <DarkModeButton/>
+                    <DarkModeButton />
 
                     <!-- Profile -->
                     <div class="flex items-center ml-3 gap-4">
                         <!-- Avatar button -->
-                        <button type="button" class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button-2" aria-expanded="false" data-dropdown-toggle="dropdown-profile">
+                        <button
+                            type="button"
+                            class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                            id="user-menu-button-2"
+                            aria-expanded="false"
+                            data-dropdown-toggle="dropdown-profile"
+                        >
                             <img
                                 class="w-8 h-8 rounded-full"
-                                :src="'https://api.dicebear.com/9.x/bottts-neutral/svg?seed=' + $page.props.auth.user.pseudo"
+                                :src="
+                                    'https://api.dicebear.com/9.x/bottts-neutral/svg?seed=' +
+                                    $page.props.auth.user.pseudo
+                                "
                                 alt="avatar"
                             />
                         </button>
-                        
+
                         <!-- Dropdown menu -->
-                        <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown-profile">
+                        <div
+                            class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600"
+                            id="dropdown-profile"
+                        >
                             <!-- Profile info -->
                             <div class="px-4 py-3" role="none">
-                                <p class="text-sm text-gray-900 dark:text-white" role="none">
+                                <p
+                                    class="text-sm text-gray-900 dark:text-white"
+                                    role="none"
+                                >
                                     {{ $page.props.auth.user.name }}
                                 </p>
-                                <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
+                                <p
+                                    class="text-sm font-medium text-gray-900 truncate dark:text-gray-300"
+                                    role="none"
+                                >
                                     {{ $page.props.auth.user.email }}
                                 </p>
                             </div>
@@ -62,13 +97,22 @@ const logout = () => {
                             <!-- Links -->
                             <ul class="py-1" role="none">
                                 <li>
-                                    <Link :href="route('profile.edit')" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white">
+                                    <Link
+                                        :href="route('profile.edit')"
+                                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+                                    >
                                         Profil
                                     </Link>
                                 </li>
                                 <li>
-                                    <form @submit.prevent="logout" class="block hover:bg-gray-100 dark:hover:bg-gray-600">
-                                        <button type="submit" class="px-4 py-2 text-sm text-gray-700  dark:text-gray-300 dark:hover:text-white">
+                                    <form
+                                        @submit.prevent="logout"
+                                        class="block hover:bg-gray-100 dark:hover:bg-gray-600"
+                                    >
+                                        <button
+                                            type="submit"
+                                            class="px-4 py-2 text-sm text-gray-700 dark:text-gray-300 dark:hover:text-white"
+                                        >
                                             Déconnexion
                                         </button>
                                     </form>
--- resources/js/Components/Navs/Sidebar.vue
+++ resources/js/Components/Navs/Sidebar.vue
@@ -1,47 +1,100 @@
 <script setup>
-import NavLink from '@/Components/Navs/NavLink.vue';
-import DropdownNavLink from '@/Components/Navs/DropdownNavLink.vue';
+import NavLink from "@/Components/Navs/NavLink.vue";
+import DropdownNavLink from "@/Components/Navs/DropdownNavLink.vue";
 </script>
 
 <template>
-    <aside id="sidebar" class="fixed top-0 left-0 z-40 flex flex-col flex-shrink-0 w-64 h-full pt-16 font-normal duration-75 lg:flex transition-transform -translate-x-full lg:translate-x-0" aria-label="Sidebar">
-        <div class="relative flex flex-col flex-1 min-h-0 pt-0 bg-white border-r border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+    <aside
+        id="sidebar"
+        class="fixed top-0 left-0 z-40 flex flex-col flex-shrink-0 w-64 h-full pt-16 font-normal duration-75 lg:flex transition-transform -translate-x-full lg:translate-x-0"
+        aria-label="Sidebar"
+    >
+        <div
+            class="relative flex flex-col flex-1 min-h-0 pt-0 bg-white border-r border-gray-200 dark:bg-gray-800 dark:border-gray-700"
+        >
             <div class="flex flex-col flex-1 pt-5 pb-4 overflow-y-auto">
-                <div class="flex-1 px-3 space-y-1 bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
+                <div
+                    class="flex-1 px-3 space-y-1 bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700"
+                >
                     <ul class="pb-2 space-y-2">
-                        <NavLink routename="dashboard" icon="chart-column" iconColor="purple" activeFor="Dashboard">
+                        <NavLink
+                            routename="dashboard"
+                            icon="chart-column"
+                            iconColor="purple"
+                            activeFor="Dashboard"
+                        >
                             Dashboard
                         </NavLink>
 
-                        <NavLink routename="players.index" icon="user" iconColor="blue" activeFor="Players">
+                        <NavLink
+                            routename="players.index"
+                            icon="user"
+                            iconColor="blue"
+                            activeFor="Players"
+                        >
                             Joueurs
                         </NavLink>
 
-                        <NavLink routename="teams.index" icon="users" iconColor="yellow" activeFor="Teams">
+                        <NavLink
+                            routename="teams.index"
+                            icon="users"
+                            iconColor="yellow"
+                            activeFor="Teams"
+                        >
                             Équipes
                         </NavLink>
 
-                        <NavLink routename="tournaments.index" icon="trophy" iconColor="red" activeFor="Tournaments">
+                        <NavLink
+                            routename="tournaments.index"
+                            icon="trophy"
+                            iconColor="red"
+                            activeFor="Tournaments"
+                        >
                             Tournois
                         </NavLink>
 
-                        <NavLink routename="payments.index" icon="money-bill" iconColor="green" activeFor="Payments">
+                        <NavLink
+                            routename="payments.index"
+                            icon="money-bill"
+                            iconColor="green"
+                            activeFor="Payments"
+                        >
                             Paiements
                         </NavLink>
-                        
-                        <NavLink routename="games.index" icon="gamepad" iconColor="gray" activeFor="Games">
+
+                        <NavLink
+                            routename="games.index"
+                            icon="gamepad"
+                            iconColor="gray"
+                            activeFor="Games"
+                        >
                             Jeux
                         </NavLink>
 
-                        <NavLink routename="faqs.index" icon="question" iconColor="purple" activeFor="Faqs">
+                        <NavLink
+                            routename="faqs.index"
+                            icon="question"
+                            iconColor="purple"
+                            activeFor="Faqs"
+                        >
                             FAQ
                         </NavLink>
 
-                        <NavLink routename="logs.index" icon="file-pen" iconColor="red" activeFor="Logs">
+                        <NavLink
+                            routename="logs.index"
+                            icon="file-pen"
+                            iconColor="red"
+                            activeFor="Logs"
+                        >
                             Logs
                         </NavLink>
 
-                        <NavLink routename="settings.index" icon="cog" iconColor="gray" activeFor="Settings">
+                        <NavLink
+                            routename="settings.index"
+                            icon="cog"
+                            iconColor="gray"
+                            activeFor="Settings"
+                        >
                             Paramètres
                         </NavLink>
                     </ul>
@@ -50,5 +103,8 @@ import DropdownNavLink from '@/Components/Navs/DropdownNavLink.vue';
         </div>
     </aside>
 
-    <div class="fixed inset-0 z-10 hidden bg-gray-900/50 dark:bg-gray-900/90" id="sidebarBackdrop"></div>
+    <div
+        class="fixed inset-0 z-10 hidden bg-gray-900/50 dark:bg-gray-900/90"
+        id="sidebarBackdrop"
+    ></div>
 </template>

aa4c1bc8ad9dc44d5f3f6c9cd31953e6f3e0b924
Thibaud P <25123473+ThobsChoucroute@users.noreply.github.com>
2024-09-19 21:37:59 +0200

    InputLabel and Error can translate

--- resources/js/Components/Forms/InputError.vue
+++ resources/js/Components/Forms/InputError.vue
@@ -9,7 +9,7 @@ defineProps({
 <template>
     <div v-show="message">
         <p class="text-sm text-red-600 dark:text-red-400">
-            {{ message }}
+            {{ __(message) }}
         </p>
     </div>
 </template>
--- resources/js/Components/Forms/InputLabel.vue
+++ resources/js/Components/Forms/InputLabel.vue
@@ -14,7 +14,7 @@ defineProps({
     <label
         class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
     >
-        <span v-if="value">{{ value }}</span>
+        <span v-if="value">{{ __(value) }}</span>
         <span v-else><slot /></span>
         <span
             class="text-red-500 dark:text-red-400 font-semibold"

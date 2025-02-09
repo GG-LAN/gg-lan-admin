eb76977f5d282af72257dbdc655014f60ee19917
Thibaud P <25123473+ThobsChoucroute@users.noreply.github.com>
2025-02-09 15:05:06 +0100

    Update TournamentController.php

--- app/Http/Controllers/Api/TournamentController.php
+++ app/Http/Controllers/Api/TournamentController.php
@@ -8,8 +8,8 @@ use App\Models\Team;
 use App\Models\Tournament;
 use App\Models\TournamentPrice;
 use App\Models\User;
-use Auth;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
 
 class TournamentController extends Controller
 {

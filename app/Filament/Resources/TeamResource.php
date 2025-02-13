572cbf2fcedb6c0bf76bbd9cd125291966f5eb18
Thibaud <25123473+ThobsChoucroute@users.noreply.github.com>
2025-02-13 17:44:57 +0100

    add TeamResource with List

---
+++ app/Filament/Resources/TeamResource.php
@@ -0,0 +1,115 @@
+<?php
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TeamResource\Pages;
+use App\Models\Team;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Table;
+use Illuminate\Support\Str;
+
+class TeamResource extends Resource
+{
+    protected static ?string $model = Team::class;
+
+    protected static ?string $navigationIcon = 'fas-users';
+
+    protected static ?int $navigationSort = 2;
+
+    public static function getModelLabel(): string
+    {
+        return __("Team");
+    }
+
+    public static function getPluralModelLabel(): string
+    {
+        return __("Teams");
+    }
+
+    public static function getNavigationGroup(): string
+    {
+        return __("Players");
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->defaultPaginationPageOption(5)
+            ->columns([
+                TextColumn::make('name')
+                    ->label(__("Team"))
+                    ->searchable(),
+                TextColumn::make('description')
+                    ->translateLabel()
+                    ->limit(30)
+                    ->searchable(),
+                TextColumn::make('created_at')
+                    ->translateLabel()
+                    ->dateTime("d/m/Y")
+                    ->sortable(),
+                TextColumn::make("players_by_team")
+                    ->label(__("Player / Team"))
+                    ->state(function (Team $team) {
+                        $test = $team->users->count() . " / " . $team->tournament->game->places;
+                        return $test;
+                    }),
+                TextColumn::make("registration_state")
+                    ->label(__("Status"))
+                    ->badge()
+                    ->color(fn(string $state): string => match ($state) {
+                        "registered"                      => "success",
+                        "pending"                         => "warning",
+                        "not_full"                        => "danger",
+                    })
+                    ->formatStateUsing(fn(string $state): string => __(Str::ucfirst($state)))
+                    ->sortable(),
+                TextColumn::make('updated_at')
+                    ->translateLabel()
+                    ->dateTime("d/m/Y")
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                SelectFilter::make("registration_state")
+                    ->label(__("Status"))
+                    ->options([
+                        "registered" => __("Registered"),
+                        "pending"    => __("Pending"),
+                        "not_full"   => __("Not_full"),
+                    ])
+                    ->multiple(),
+            ])
+            ->actions([
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTeams::route('/'),
+        ];
+    }
+}

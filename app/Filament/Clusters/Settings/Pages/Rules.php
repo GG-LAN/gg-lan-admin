a81e40dde73a5a4a2860525280abbc366c47b7b9
Thibaud <25123473+ThobsChoucroute@users.noreply.github.com>
2025-02-14 10:25:24 +0100

    add rules editor

--- app/Filament/Clusters/Settings/Pages/Rules.php
+++ app/Filament/Clusters/Settings/Pages/Rules.php
@@ -2,10 +2,20 @@
 namespace App\Filament\Clusters\Settings\Pages;
 
 use App\Filament\Clusters\Settings;
+use App\Models\Rule;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Forms\Form;
+use Filament\Notifications\Notification;
 use Filament\Pages\Page;
+use Illuminate\Contracts\Support\Htmlable;
 
-class Rules extends Page
+class Rules extends Page implements HasForms
 {
+    use InteractsWithForms;
+
     protected static ?string $navigationIcon = 'fas-align-left';
 
     protected static string $view = 'filament.clusters.settings.pages.rules';
@@ -13,4 +23,49 @@ class Rules extends Page
     protected static ?string $cluster = Settings::class;
 
     protected static ?int $navigationSort = 1;
+
+    public array $data = [];
+
+    public function getTitle(): string | Htmlable
+    {
+        return __("Settings");
+    }
+
+    public static function getNavigationLabel(): string
+    {
+        return __("Rules");
+    }
+
+    public function mount(): void
+    {
+        $this->form->fill([
+            "content" => Rule::firstOrCreate()->content,
+        ]);
+    }
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Grid::make()
+                    ->id("richeditor")
+                    ->columns(1)
+                    ->schema([
+                        RichEditor::make("content")
+                            ->label(__("Rules"))
+                            ->extraInputAttributes(["style" => "max-height: 30rem;"]),
+                    ]),
+            ])
+            ->statePath('data');
+    }
+
+    public function update(): void
+    {
+        Rule::first()->update($this->form->getState());
+
+        Notification::make()
+            ->title(__("responses.rules.updated"))
+            ->success()
+            ->send();
+    }
 }
--- resources/views/filament/clusters/settings/pages/rules.blade.php
+++ resources/views/filament/clusters/settings/pages/rules.blade.php
@@ -1,3 +1,11 @@
 <x-filament-panels::page>
+    <form wire:submit.prevent="update">
+        {{ $this->form }}
 
+        <div class="flex items-center justify-end">
+            <x-filament::button color="success" type="submit" size="lg" class="mt-4">
+                {{ __("Update") }}
+            </x-filament::button>
+        </div>
+    </form>
 </x-filament-panels::page>

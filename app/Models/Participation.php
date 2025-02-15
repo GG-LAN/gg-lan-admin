365b7fcfaaec2e95d3b5dea9a50a08477913fa9d
Thibaud P <25123473+ThobsChoucroute@users.noreply.github.com>
2025-02-15 12:40:16 +0100

    Update Participation.php

--- app/Models/Participation.php
+++ app/Models/Participation.php
@@ -31,16 +31,18 @@ class Participation extends Model
 
     public static function register(User $user, Tournament $tournament, ?Team $team = null): Participation
     {
-        return self::updateOrCreate(
-            [
-                "user_id"       => $user->id,
-                "tournament_id" => $tournament->id,
-            ],
-            [
-                "team_id" => $team ? $team->id : null,
-                "status"  => $team ? $team->registration_state : "registered",
-            ]
-        );
+        $participation = self::firstOrNew([
+            "user_id"       => $user->id,
+            "tournament_id" => $tournament->id,
+        ]);
+
+        $participation->user()->associate($user);
+        $participation->team()->associate($team);
+        $participation->tournament()->associate($tournament);
+
+        $participation->save();
+
+        return $participation;
     }
 
     public static function unregister(User $user, Tournament $tournament, ?Team $team = null): void

3e468b995f0918e33d1f68cefaa8b49eeaf480ad
Thibaud P <25123473+ThobsChoucroute@users.noreply.github.com>
2025-02-15 11:15:50 +0100

    add cover image upload

--- app/Filament/Clusters/Settings/Pages/CoverImage.php
+++ app/Filament/Clusters/Settings/Pages/CoverImage.php
@@ -2,10 +2,23 @@
 namespace App\Filament\Clusters\Settings\Pages;
 
 use App\Filament\Clusters\Settings;
+use App\Models\CoverImage as ModelsCoverImage;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Forms\Form;
+use Filament\Notifications\Notification;
 use Filament\Pages\Page;
+use Illuminate\Contracts\Support\Htmlable;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Drivers\Gd\Driver;
+use Intervention\Image\ImageManager;
+use Livewire\Features\SupportFileUploads\TemporaryUploadedFile;
 
-class CoverImage extends Page
+class CoverImage extends Page implements HasForms
 {
+    use InteractsWithForms;
+
     protected static ?string $navigationIcon = 'fas-panorama';
 
     protected static string $view = 'filament.clusters.settings.pages.cover-image';
@@ -13,4 +26,70 @@ class CoverImage extends Page
     protected static ?string $cluster = Settings::class;
 
     protected static ?int $navigationSort = 4;
+
+    public array $data = [];
+
+    public bool $fileHasChanged = false;
+
+    public function getTitle(): string | Htmlable
+    {
+        return __("Settings");
+    }
+
+    public static function getNavigationLabel(): string
+    {
+        return __("Cover Image");
+    }
+
+    public function mount(): void
+    {
+        $coverImage = ModelsCoverImage::firstOrCreate();
+
+        $this->form->fill([
+            "image" => $coverImage->path,
+        ]);
+    }
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                FileUpload::make("image")
+                    ->disk("public")
+                    ->image()
+                    ->previewable(true)
+                    ->downloadable()
+                    ->maxSize(10000)
+                    ->saveUploadedFileUsing(function (TemporaryUploadedFile $file) {
+                        $manager = new ImageManager(new Driver());
+
+                        $coverImage = $manager->read($file);
+
+                        $coverImage->toWebp()->save(Storage::disk("public")->path("cover.webp"));
+
+                        $this->fileHasChanged = true;
+
+                        return "cover.webp";
+                    }),
+            ])
+            ->statePath("data");
+    }
+
+    public function update(): void
+    {
+        $coverImage = ModelsCoverImage::first();
+
+        if ($coverImage->path != null && $this->fileHasChanged) {
+            Storage::disk("public")->delete($coverImage->path);
+        }
+
+        $coverImage->path = "/" . $this->form->getState()["image"];
+
+        $coverImage->save();
+
+        Notification::make()
+            ->title(__("responses.setting.updated"))
+            ->success()
+            ->send();
+    }
 }
--- resources/views/filament/clusters/settings/pages/cover-image.blade.php
+++ resources/views/filament/clusters/settings/pages/cover-image.blade.php
@@ -1,3 +1,17 @@
 <x-filament-panels::page>
+    <div class="grid grid-cols-3 gap-4">
+        <x-filament::section class="col-span-3">
+            <h2 class="text-xl font-semibold text-gray-900 dark:text-gray-100 mb-4"> {{ __("Cover Image") }}</h2>
 
+            <form wire:submit.prevent="update">
+                {{ $this->form }}
+
+                <div class="flex items-center justify-end">
+                    <x-filament::button color="success" type="submit" form="update" size="lg" class="mt-4">
+                        <span>{{ __("Update") }}</span>
+                    </x-filament::button>
+                </div>
+            </form>
+        </x-filament::section>
+    </div>
 </x-filament-panels::page>

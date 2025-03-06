18576e65cc4f2fb910d60d2f1d2104de69a56993
Thibaud <25123473+ThobsChoucroute@users.noreply.github.com>
2025-03-06 15:06:22 +0100

    order filter tournaments by id desc

--- app/Filament/Resources/TeamResource.php
+++ app/Filament/Resources/TeamResource.php
@@ -11,6 +11,7 @@ use Filament\Tables;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Str;
 
 class TeamResource extends Resource
@@ -96,7 +97,7 @@ class TeamResource extends Resource
                 SelectFilter::make('tournament')
                     ->translateLabel()
                     ->multiple()
-                    ->relationship('tournament', 'name')
+                    ->relationship('tournament', 'name', fn(Builder $query) => $query->orderBy("id", "desc"))
                     ->preload()
                     ->default(function () {
                         return Tournament::getOpenTournaments()->pluck("id")->toArray();

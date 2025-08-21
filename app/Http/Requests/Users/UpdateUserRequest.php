9be123da71ba45edab4247e058b989a2babecb75
Thibaud P <25123473+ThobsChoucroute@users.noreply.github.com>
2025-08-21 22:01:21 +0200

    make birth_date field required for update

--- app/Http/Requests/Users/UpdateUserRequest.php
+++ app/Http/Requests/Users/UpdateUserRequest.php
@@ -27,7 +27,7 @@ class UpdateUserRequest extends FormRequest
     public function rules() {
         return [
             'pseudo' => 'required|string|max:255',
-            'birth_date' => 'date_format:Y-m-d',
+            'birth_date' => 'required|date_format:Y-m-d',
         ];
     }
 

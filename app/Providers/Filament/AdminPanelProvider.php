8567cb6418616dc8fe9579856e7309e5dde0c6fb
Thibaud <25123473+ThobsChoucroute@users.noreply.github.com>
2025-10-30 14:38:54 +0100

    change red color tone

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -117,7 +117,20 @@ class AdminPanelProvider extends PanelProvider
         ]);
 
         FilamentColor::register([
-            "primary" => Color::hex("#ff0000"),
+            "primary" => Color::Red,
+            // "primary" => [
+            //     '50'  => '#fff0f0',
+            //     '100' => '#ffdddd',
+            //     '200' => '#ffc0c0',
+            //     '300' => '#ff9494',
+            //     '400' => '#ff5757',
+            //     '500' => '#ff2323',
+            //     '600' => '#ff0000',
+            //     '700' => '#d70000',
+            //     '800' => '#b10303',
+            //     '900' => '#920a0a',
+            //     '950' => '#500000',
+            // ],
             "success" => [
                 50  => "#F3FAF7",
                 100 => "#DEF7EC",
